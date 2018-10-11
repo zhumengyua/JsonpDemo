670232498c094b56c73c71789fd5afa53e154c5e
Amanda <>
2018-10-11 21:02:51 +0800

    frist

---
+++ index.js
@@ -0,0 +1,65 @@
+﻿var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function (request, response) {
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url
+  var queryString = ''
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
+  if (path === '/') {  // 如果用户请求的是 / 路径
+    var string = fs.readFileSync('index.html', 'utf8')
+    var amount = fs.readFileSync('./db', 'utf8')
+    string = string.replace('&&&amount&&&', amount)
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.end(string)
+  } else if (path === '/style.css') {
+    var string = fs.readFileSync('./style.css')
+    var string = fs.readFileSync('./style.css', 'utf8')
+    response.setHeader('Content-Type', 'text/css')
+    response.end(string)
+  } else if (path === '/main.js') {
+    var string = fs.readFileSync('./main.js')
+    var string = fs.readFileSync('./main.js', 'utf8')
+    response.setHeader('Content-Type', 'application/javascript')
+    response.end(string)
+
+  } else if (path === '/pay') {
+    var amount = fs.readFileSync('./db', 'utf8')
+    var newAmount = amount - 1
+    fs.writeFileSync('./db', newAmount)
+    response.setHeader('Content-Type', 'application/javascript')
+    // response.setHeader('Content-Type','image/jpg')
+    response.statusCode = 200
+    response.write(`
+    alert("成功")
+    amount.innerText=amount.innerText-1`)
+    //response.write(fs.readFileSync('./dog.jpg'))
+    response.end()
+  } else {
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('啊啊啊啊我不想见到你')
+    response.end()
+  }
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
